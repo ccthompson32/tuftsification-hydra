ff1e6b73075acdd1b33c3da615af3473a599c3ec
Mike Korcynski <mkorcy@gmail.com>
2013-06-06 17:01:41 -0400

    moving fixtures into tuftsification, and the loading/refreshing of them as well

--- lib/tuftsification-hydra/engine.rb
+++ lib/tuftsification-hydra/engine.rb
@@ -1,4 +1,10 @@
 module TuftsificationHydra
   class Engine < ::Rails::Engine
+      # Load rake tasks
+      rake_tasks do
+        Dir.glob(File.join(File.expand_path('../', File.dirname(__FILE__)),'railties', '*.rake')).each do |railtie|
+          load railtie
+        end
+      end
   end
 end
