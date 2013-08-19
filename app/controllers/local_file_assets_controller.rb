2bb2e9de2382ffd4e2d4dba70b4072c0dc5b7c06
Mike Korcynski <mkorcy@gmail.com>
2013-08-19 17:13:03 -0400

    webm changes

--- app/controllers/local_file_assets_controller.rb
+++ app/controllers/local_file_assets_controller.rb
@@ -568,4 +568,33 @@ From file_assets/_new.html.haml
     end
   end
 
+
+  def showWebm
+      @file_asset = FileAsset.find(params[:id])
+      if (@file_asset.nil?)
+        logger.warn("No such file asset: " + params[:id])
+        flash[:notice]= "No such file asset."
+        redirect_to(:action => 'index', :q => nil, :f => nil)
+      else
+        # get containing object for this FileAsset
+        #pid = @file_asset.container_id
+        pid = params[:id]
+        @downloadable = false
+        # A FileAsset is downloadable iff the user has read or higher access to a parent
+        @response, @permissions_solr_document = get_solr_response_for_doc_id(pid)
+        if reader?
+          @downloadable = true
+        end
+
+        mapped_model_names = ModelNameHelper.map_model_names(@file_asset.relationships(:has_model))
+
+        if (mapped_model_names.include?("info:fedora/afmodel:TuftsVideo"))
+          if @file_asset.datastreams.include?("Access.webm")
+            send_file(convert_url_to_local_path(@file_asset.datastreams["Access.webm"].dsLocation), :type => 'video/webm')
+          end
+        end
+
+      end
+    end
+
 end
