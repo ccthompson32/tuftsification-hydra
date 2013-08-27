78ae18df6316d9dd291914d6f7bf42d6ecda8f0d
WillHumphries <will.d.humphries@gmail.com>
2013-08-27 10:10:11 -0400

    identify ead datastream properly

--- app/models/tufts_ead.rb
+++ app/models/tufts_ead.rb
@@ -2,7 +2,7 @@ class TuftsEAD < TuftsBase
 
   # Tufts specific needed metadata streams
   # This is unusual, because it's an external datastream, but it is loaded by fedora
-  #has_metadata :name => "Archival.xml", :type => TuftsEADMeta
+  has_metadata :name => "Archival.xml", :type => TuftsEADMeta
 
   def self.to_class_uri
     'info:fedora/cm:Text.EAD'
