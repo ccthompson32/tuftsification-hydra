fabce5d3e280baf940b5c24ede6e6854c9fac3df
Mike Korcynski <mkorcy@gmail.com>
2013-09-11 10:58:03 -0400

    moved render video path method here from module

--- app/helpers/video_delivery_helper.rb
+++ app/helpers/video_delivery_helper.rb
@@ -1,3 +1,8 @@
 module VideoDeliveryHelper
-  include Tufts::VideoRenderingMethods
+  def self.render_video_path(path, type, pid)
+      # no interest in modding the path here but we do want to do it in corpora/sadl
+      # so I'm trying to give us a point to override this without bringing the whole
+      # controller in to the project
+      return path
+  end
 end
