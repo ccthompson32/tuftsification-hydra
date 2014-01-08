2b9d3b96a0876b5fa834b709527fbc4422a3cf5a
Mike Korcynski <mkorcy@gmail.com>
2014-01-08 17:33:06 -0500

    specify load order of js

--- app/assets/javascripts/tuftsification-hydra.js
+++ app/assets/javascripts/tuftsification-hydra.js
@@ -3,5 +3,7 @@
  *= require jwplayer6/jwplayer.js
  *= require jwplayer6/jwplayer.html5.js
  *= require jwplayer6/extras.js
- *= require_directory ./media_player
+ *= require media_player/media_player.js
+ *= require media_player/transcript_scroller.js
+ *= require media_player/tabs.js
 */
